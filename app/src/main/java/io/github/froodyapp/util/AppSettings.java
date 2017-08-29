c3b8ed6bfdcd66f7e5d3f308e398eb51cd34dbb5
Gregor Santner <gsantner@mailbox.org>
2017-08-29 14:42:55 +0200

    Include String key as parameter in AppSettingsBase

--- app/src/main/java/io/github/froodyapp/util/AppSettings.java
+++ app/src/main/java/io/github/froodyapp/util/AppSettings.java
@@ -32,7 +32,7 @@ public class AppSettings extends AppSettingsBase {
     //## Settings options
     //############################################
     public long getFroodyUserId() {
-        return getLong(_prefApp, R.string.pref_key__user_id, -1);
+        return getLong(R.string.pref_key__user_id, -1);
     }
 
     public boolean hasFroodyUserId() {
@@ -49,20 +49,20 @@ public class AppSettings extends AppSettingsBase {
     }
 
     public void setFroodyUserId(long value) {
-        setLong(_prefApp, R.string.pref_key__user_id, value);
+        setLong(R.string.pref_key__user_id, value);
     }
 
     public boolean isDevDebugModeEnabled() {
-        return getBool(_prefApp, R.string.pref_key__dev_mode_debugging, false);
+        return getBool(R.string.pref_key__dev_mode_debugging, false);
     }
 
     public void setDevDebugModeEnabled(boolean value) {
-        setBool(_prefApp, R.string.pref_key__dev_mode_debugging, value);
+        setBool(R.string.pref_key__dev_mode_debugging, value);
     }
 
 
     public String getFroodyServer() {
-        return getString(_prefApp, R.string.pref_key__froody_server, rstr(R.string.server_default));
+        return getString(R.string.pref_key__froody_server, rstr(R.string.server_default));
     }
 
     public boolean hasLastMapLocation() {
@@ -70,61 +70,61 @@ public class AppSettings extends AppSettingsBase {
     }
 
     public double getLastMapLocationLatitude() {
-        return getDouble(_prefApp, R.string.pref_key__last_map_location_latitude, 0);
+        return getDouble(R.string.pref_key__last_map_location_latitude, 0);
     }
 
     public double getLastMapLocationLongitude() {
-        return getDouble(_prefApp, R.string.pref_key__last_map_location_longitude, 0);
+        return getDouble(R.string.pref_key__last_map_location_longitude, 0);
     }
 
     public int getLastMapLocationZoom() {
-        return getInt(_prefApp, R.string.pref_key__last_map_location_zoom, 0);
+        return getInt(R.string.pref_key__last_map_location_zoom, 0);
     }
 
     public void setLastMapLocation(double lat, double lng, int zoom) {
-        setDouble(_prefApp, R.string.pref_key__last_map_location_latitude, lat);
-        setDouble(_prefApp, R.string.pref_key__last_map_location_longitude, lng);
-        setInt(_prefApp, R.string.pref_key__last_map_location_zoom, zoom);
+        setDouble(R.string.pref_key__last_map_location_latitude, lat);
+        setDouble(R.string.pref_key__last_map_location_longitude, lng);
+        setInt(R.string.pref_key__last_map_location_zoom, zoom);
     }
 
     public int getLastCertification() {
-        return getInt(_prefApp, R.string.pref_key__entry__last_certification, 0);
+        return getInt(R.string.pref_key__entry__last_certification, 0);
     }
 
     public void setLastCertification(int value) {
-        setInt(_prefApp, R.string.pref_key__entry__last_certification, value);
+        setInt(R.string.pref_key__entry__last_certification, value);
     }
 
     public String getLastContactInfo() {
-        return getString(_prefApp, R.string.pref_key__entry__last_contact_info, "");
+        return getString(R.string.pref_key__entry__last_contact_info, "");
     }
 
     public void setLastContactInfo(String value) {
-        setString(_prefApp, R.string.pref_key__entry__last_contact_info, value);
+        setString(R.string.pref_key__entry__last_contact_info, value);
     }
 
     public int getLastDistribution() {
-        return getInt(_prefApp, R.string.pref_key__entry__last_distribution, 0);
+        return getInt(R.string.pref_key__entry__last_distribution, 0);
     }
 
     public void setLastSelectedEntryTypes(ArrayList<Integer> values) {
-        setIntList(_prefApp, R.string.pref_key__entry__last_selected_entry_types, values);
+        setIntList(R.string.pref_key__entry__last_selected_entry_types, values);
     }
 
     public ArrayList<Integer> getLastSelectedEntryTypes() {
-        return getIntList(_prefApp, R.string.pref_key__entry__last_selected_entry_types);
+        return getIntList(R.string.pref_key__entry__last_selected_entry_types);
     }
 
     public void setLastDistribution(int value) {
-        setInt(_prefApp, R.string.pref_key__entry__last_distribution, value);
+        setInt(R.string.pref_key__entry__last_distribution, value);
     }
 
     public boolean getAllowLocationListeningGps() {
-        return getBool(_prefApp, R.string.pref_key__allow_location_listening_gps, true);
+        return getBool(R.string.pref_key__allow_location_listening_gps, true);
     }
 
     public boolean getAllowLocationListeningNetwork() {
-        return getBool(_prefApp, R.string.pref_key__allow_location_listening_net, true);
+        return getBool(R.string.pref_key__allow_location_listening_net, true);
     }
 
     public boolean getAllowLocationListeningAny() {
@@ -132,37 +132,37 @@ public class AppSettings extends AppSettingsBase {
     }
 
     public void setLastFoundLocation(String geohash, String address) {
-        setString(_prefApp, R.string.pref_key__last_found_location_address, address);
-        setString(_prefApp, R.string.pref_key__last_found_location_geohash, geohash);
+        setString(R.string.pref_key__last_found_location_address, address);
+        setString(R.string.pref_key__last_found_location_geohash, geohash);
     }
 
     public String[] getLastFoundLocation() {
         return new String[]{
-                getString(_prefApp, R.string.pref_key__last_found_location_geohash, ""),
-                getString(_prefApp, R.string.pref_key__last_found_location_address, "")
+                getString(R.string.pref_key__last_found_location_geohash, ""),
+                getString(R.string.pref_key__last_found_location_address, "")
         };
     }
 
     public boolean isAppFirstStart() {
-        return getBool(_prefApp, R.string.pref_key__app_first_start, true);
+        return getBool(R.string.pref_key__app_first_start, true);
     }
 
     public void setAppFirstStart(boolean value) {
-        setBool(_prefApp, R.string.pref_key__app_first_start, value);
+        setBool(R.string.pref_key__app_first_start, value);
     }
 
     public boolean isAppFirstStartCurrentVersion() {
-        int value = getInt(_prefApp, R.string.pref_key__app_first_start_current_version, -1);
-        setInt(_prefApp, R.string.pref_key__app_first_start_current_version, BuildConfig.VERSION_CODE);
+        int value = getInt(R.string.pref_key__app_first_start_current_version, -1);
+        setInt(R.string.pref_key__app_first_start_current_version, BuildConfig.VERSION_CODE);
         return value != BuildConfig.VERSION_CODE && !BuildConfig.IS_TEST_BUILD;
     }
 
     public void setEntryTypeSelectionDialogTabLastUsed(int value) {
-        setInt(_prefApp, R.string.pref_key__entry_type_selection_dialog__tab_last_used, value);
+        setInt(R.string.pref_key__entry_type_selection_dialog__tab_last_used, value);
     }
 
     public int getEntryTypeSelectionDialogTabLastUsed() {
-        return getInt(_prefApp, R.string.pref_key__entry_type_selection_dialog__tab_last_used, 0);
+        return getInt(R.string.pref_key__entry_type_selection_dialog__tab_last_used, 0);
     }
 
     public boolean isNetworkHttpProxyEnabled() {
